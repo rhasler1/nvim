8a2447589537c9cda0e3b4ef7cb2fbfb70adfd7c
rhasler1 <rhasler@luc.edu>
2026-02-23 13:58:03 -0600

    chore: add mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -3,6 +3,9 @@
 -- Map leader
 vim.g.mapleader = ' '
 
+-- TODO: [2/23/26] Add a mapping for the substitute command
+-- e.g., :%s/old_word/new_word/g
+
 -- Test map leader
 -- arg0: 'n'                = mapping only works in this mode ('n'=normal)
 -- arg1: '<leader>1'        = key combination to trigger command or function
@@ -22,4 +25,11 @@ vim.keymap.set(
     '<leader>yy',
     '\"+yy',
     { desc = 'Yank current line to system clipboard' }
+)
+
+vim.keymap.set(
+    'n',
+    '<leader>p',
+    '"+p',
+    { desc = 'Paste from system clipboard' }
 )
